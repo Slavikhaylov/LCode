95f05c95be8399f24a89398d39eb33660b59e7d0
Slavikhaylov <hslavik627@gmail.com>
2022-04-02 13:05:02 +0500

    nav bar

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+
+
+const language = document.querySelector('.language')
+document.addEventListener('click' , function(e){
+    language.classList.remove('lang-active')
+    if(e.target.closest('.current-lang')){
+        language.classList.add('lang-active')
+    }
+    
+
+})
